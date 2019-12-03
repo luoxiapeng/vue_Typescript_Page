1c632f31ca9d1abbee8f5e2bd9223ae4f9e966e8
luoxiapeng <949123073@qq.com>
2019-12-03 08:39:44 +0800

    构建框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private haha: string = "1111";
+  created() {
+    // alert(this.haha);
+  }
+  private fn() {
+    return;
+  }
+}
 </script>

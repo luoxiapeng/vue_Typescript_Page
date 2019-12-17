682f018e3afc4fc4272b3c7578ab009429d5cd48
luoxiapeng <949123073@qq.com>
2019-12-17 23:10:31 +0800

    更行分类功能

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ import 'lib-flexible/flexible'
 import 'vant/lib/index.css'
 import 'amfe-flexible/index.js'
 
-import { Button, Row, Col,Swipe, SwipeItem,Lazyload,Tabbar, TabbarItem, Tab, Tabs } from 'vant'
+import { Button, Row, Col,Swipe, SwipeItem,Lazyload,Tabbar, TabbarItem, Tab, Tabs,TreeSelect  } from 'vant'
 Vue.use(Button)
   .use(Row)
   .use(Col)
   .use(Swipe).use(SwipeItem)
   .use(Lazyload).use(Tabbar)
-  .use(TabbarItem).use(Tab)
-  .use(Tabs);;
+  .use(TabbarItem).use(Tab).use(TreeSelect)
+  .use(Tabs);
 
 Vue.config.productionTip = false
 new Vue({
--- src/views/Classify.vue
+++ src/views/Classify.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    分类
+  <div class="Classify-containt">
+   <van-tree-select
+     height="55vw"
+    :items="items"
+    :main-active-index.sync="activeIndex"
+  >
+  <template slot="content">
+    <!-- <van-image v-if="activeIndex === 0" src="https://img.yzcdn.cn/vant/apple-1.jpg" /> -->
+    <div v-if="activeIndex === 0">11111</div>
+    <van-image v-if="activeIndex === 1" src="https://img.yzcdn.cn/vant/apple-2.jpg" />
+  </template>
+</van-tree-select>
 
   </div>
 </template>
@@ -13,10 +23,40 @@ import { Component, Vue } from "vue-property-decorator";
   }
 })
 export default class Classify extends Vue{
+   data() {
+    return {
+      activeIndex: 0,
+      items: [
+        { text: '分组 1' }, 
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 1' }, 
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 1' }, 
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+        { text: '分组 2' },
+      ]
+    };
+  }
   
 }
 </script>
 
 <style lang="scss" scoped>
+.Classify-containt{
+  height:100%;
+  width:100%;
+}
 
 </style>
+
